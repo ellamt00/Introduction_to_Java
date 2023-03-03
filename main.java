438ce76e1bc7df61d9f5f966d1f9e8e894c72a28
emma <ellamt00@estudiantes.unileon.es>
2023-03-03 09:47:45 +0100

    week01 (main)

--- main.java
+++ main.java
@@ -1,1 +1,20 @@
-//hola
+//Introduction to java - UPCE
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		week01();
+
+		
+		
+	}
+
+    private static void week01() {
+
+        //name
+        System.out.println("\nMy name is Emma, i study at the university of Leon (Spain) and im studying computer engineering\n");
+    }
+	
+
+}
